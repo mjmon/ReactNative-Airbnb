7ffddfc5651e752943108388f4e5d10de91234eb
Michael James G. Montesclaro <montesclaro.mj@Networks-iMac.local>
2019-12-03 18:32:45 +0800

    added flex in container style

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ export default class App extends Component{
 	render() {
 		return (
 			<View style={styles.container}>
-				<Text></Text>
+				<Text>Hello World</Text>
 			</View>
 		)
 	}
@@ -25,6 +25,7 @@ export default class App extends Component{
 
 const styles = StyleSheet.create({
     container: {
+       flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
     }
