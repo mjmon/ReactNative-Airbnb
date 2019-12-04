86280b236ea5d76e05d18184c096f8da1560b7c2
Michael James G. Montesclaro <montesclaro.mj@Networks-iMac.local>
2019-12-04 17:00:35 +0800

    tab icons added

--- App.js
+++ App.js
@@ -10,15 +10,18 @@ import React, {Component} from 'react';
 import {
   StyleSheet,
   View,
-  Text
+  Text,
+  Image
 } from 'react-native';
 import {createBottomTabNavigator} from 'react-navigation-tabs'
 import { createAppContainer } from 'react-navigation'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 import Explore from './screens/Explore'
 import Saved from './screens/Saved'
 import Inbox from './screens/Inbox'
 import Trips from './screens/Trips'
+import Profile from './screens/Profile'
 
 class App extends Component{
 	render() {
@@ -32,16 +35,49 @@ class App extends Component{
 
 const TabNavigator = createBottomTabNavigator({
    Explore: {
-      screen: Explore
+      screen: Explore,
+      navigationOptions: {
+         tabBarLabel: 'EXPLORE',
+         tabBarIcon: ({tintColor}) => (
+            <Icon name="ios-search" color={tintColor} size={24}/>
+         )
+      }
    },
    Saved: {
-      screen: Saved
+      screen: Saved,
+      navigationOptions: {
+         tabBarLabel: 'SAVED',
+         tabBarIcon: ({tintColor}) => (
+            <Icon name="ios-heart-empty" color={tintColor} size={24}/>
+         )
+      }
    },
    Trips: {
-      screen: Trips
+      screen: Trips,
+      navigationOptions: {
+         tabBarLabel: 'TRIPS',
+         tabBarIcon: ({tintColor}) => (
+            <Image source={require('./assets/airbnb.png')} style={{height: 24, width:24, tintColor: tintColor}}/>
+         )
+      }
    },
    Inbox: {
-      screen: Inbox
+      screen: Inbox,
+      navigationOptions: {
+         tabBarLabel: 'INBOX',
+         tabBarIcon: ({tintColor}) => (
+            <Icon name="ios-chatboxes" color={tintColor} size={24}/>
+         )
+      }
+   },
+   Profile: {
+      screen: Profile,
+      navigationOptions: {
+         tabBarLabel: 'PROFILE',
+         tabBarIcon: ({tintColor}) => (
+            <Icon name="ios-person" color={tintColor} size={24}/>
+         )
+      }
    }
 })
 
---
+++ screens/Profile.js
@@ -0,0 +1,28 @@
+
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text
+} from 'react-native';
+
+
+class Profile extends Component{
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text>Profile</Text>
+			</View>
+		)
+	}
+}
+
+export default Profile
+
+const styles = StyleSheet.create({
+    container: {
+       flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
