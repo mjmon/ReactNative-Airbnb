a41751859fba15dacb5485829ae589584cc12c37
Michael James G. Montesclaro <montesclaro.mj@Networks-iMac.local>
2019-12-05 19:46:47 +0800

    text above the slider images

--- screens/Explore.js
+++ screens/Explore.js
@@ -7,7 +7,8 @@ import {
   SafeAreaView,
   TextInput,
   Platform,
-  StatusBar
+  StatusBar,
+  ScrollView
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/Ionicons'
@@ -41,6 +42,15 @@ class Explore extends Component{
                      style={{height: 50, flex: 1, fontWeight: '200', marginHorizontal: 5}}/>
                </View>
             </View>
+            <ScrollView
+               scrollEventThrottle={16}
+            >
+               <View style={{flex: 1, backgroundColor: 'white', paddingTop: 20}}>
+                  <Text style={{fontSize: 24, fontWeight: '700', paddingHorizontal: 20}}>
+                     What can we help you find, James?
+                  </Text>
+               </View>
+            </ScrollView>
          </SafeAreaView>
 		)
 	}
