d37f4f9af4ddc39b9d3fa7029e0a86cc3b0a46bb
Michael James G. Montesclaro <montesclaro.mj@Networks-iMac.local>
2019-12-05 18:31:11 +0800

    layout for searchbar depending on OS

--- screens/Explore.js
+++ screens/Explore.js
@@ -5,29 +5,40 @@ import {
   View,
   Text,
   SafeAreaView,
-  TextInput
+  TextInput,
+  Platform,
+  StatusBar
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/Ionicons'
 
 class Explore extends Component{
+
+   componentWillMount() {
+      this.startHeaderHeight = 80
+      if(Platform.OS == 'android') {
+         this.startHeaderHeight = 100 + StatusBar.currentHeight
+      }
+   }
+
 	render() {
 		return (
 			<SafeAreaView style={{flex: 1}}>
-            <View style={{height: 80, backgroundColor: 'white'
-             , borderBottomWidth: 1, borderBottomColor: '#dddddd'
+            <View style={{height: this.startHeaderHeight, backgroundColor: 'white',
+              borderBottomWidth: 1, borderBottomColor: '#dddddd'
             }}>
                <View style={{
-                  flexDirection: 'row', 
-                  alignItems: 'center', 
-                  padding: 10,
-                  marginHorizontal: 20
+                  flexDirection: 'row', height: 50, alignItems: 'center', 
+                  padding: 10, marginHorizontal: 20, backgroundColor: 'white',
+                  shadowOffset: {width: 0, height: 0}, shadowColor: 'black', shadowOpacity: 0.2, //for ios
+                  elevation: 3, //for android
+                  marginTop: Platform.OS == 'android'? 30: null
                }}>
                   <Icon name='ios-search' size={20}/>
                   <TextInput
                      placeholderTextColor='grey'
                      placeholder='Try Cebu City' 
-                     style={{flex: 1, fontWeight: '700', backgroundColor: 'white'}}/>
+                     style={{height: 50, flex: 1, fontWeight: '200', marginHorizontal: 5}}/>
                </View>
             </View>
          </SafeAreaView>
